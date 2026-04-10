bfbaa690a5f6f48ee60e774a085076c0b4146bae
sadra soleimani <sadrasadrasadra6@gmail.com>
2026-04-10 18:56:10 +0330

    build: sign block
    Signed-off-by: sadra soleimani <sadrasadrasadra6@gmail.com>

--- types/block.go
+++ types/block.go
@@ -3,9 +3,17 @@ package types
 import (
 	"crypto/sha256"
 
+	"github.com/sadrax4/crypto-graphy/crypto"
 	proto "github.com/sadrax4/crypto-graphy/github.com/sadrax4/crypto-graphy"
 	pb "google.golang.org/protobuf/proto"
 )
+
+func SignBlock(
+	pk *crypto.PrivateKey,
+	block *proto.Block,
+) *crypto.Signature {
+	return pk.Sign(HashBlock(block))
+}
 
 func HashBlock(block *proto.Block) []byte {
 	b, err := pb.Marshal(block)
