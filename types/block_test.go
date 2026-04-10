d76612a3277857aa97caa7a8955575930fc9e7c8
sadra soleimani <sadrasadrasadra6@gmail.com>
2026-04-10 18:36:13 +0330

    test: hash block
    Signed-off-by: sadra soleimani <sadrasadrasadra6@gmail.com>

--- types/block_test.go
+++ types/block_test.go
@@ -1,1 +1,14 @@
 package types
+
+import (
+	"testing"
+
+	"github.com/sadrax4/crypto-graphy/utils"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHashBlock(t *testing.T) {
+	block := utils.RandomBlockGen()
+	hashBlock := HashBlock(block)
+	assert.Equal(t, 32, len(hashBlock))
+}
